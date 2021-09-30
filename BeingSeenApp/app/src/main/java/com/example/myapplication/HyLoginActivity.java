9d5afcac0c008bc1788c67f59b50bc6342fafb12
Ansel-Hong <anselhonggibby@gmail.com>
2021-09-30 19:51:35 +0800

    Added navigation from HyLogin to HyUserInterface (temporary) so that user stories related to homeless youth profile can be worked on

--- BeingSeenApp/app/src/main/java/com/example/myapplication/HyLoginActivity.java
+++ BeingSeenApp/app/src/main/java/com/example/myapplication/HyLoginActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HyLoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class HyLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hy_login);
+
+
+        //Login Button code
+        final Button button = (Button) findViewById(R.id.HyLoginButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(),HyUserInterfaceActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
