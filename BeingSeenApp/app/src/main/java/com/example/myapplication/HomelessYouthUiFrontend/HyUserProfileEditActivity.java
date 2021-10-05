78397aeab687e574f1e3bdb6f9c5b02047eb7d83
Ansel-Hong <anselhonggibby@gmail.com>
2021-10-05 15:46:47 +0800

    Implemented image selection for profile (need to edit as api does not work for some devices)

--- BeingSeenApp/app/src/main/java/com/example/myapplication/HomelessYouthUiFrontend/HyUserProfileEditActivity.java
+++ BeingSeenApp/app/src/main/java/com/example/myapplication/HomelessYouthUiFrontend/HyUserProfileEditActivity.java
@@ -1,15 +1,34 @@
 package com.example.myapplication.HomelessYouthUiFrontend;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 public class HyUserProfileEditActivity extends AppCompatActivity {
+public ImageView pick;
+public static final int CAMERA_REQUEST = 100;
+public static final int STORAGE_REQUEST = 101;
+String cameraPermission[];
+String storagePermission[];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +60,108 @@ public class HyUserProfileEditActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+        // Part of image picking code inspired by https://www.youtube.com/watch?v=jOr5kpgFpzA&ab_channel=AtifPervaiz
+        // and https://www.youtube.com/watch?v=2tRw6Q2JXGo&ab_channel=coursecode
+
+        // get camera code
+        cameraPermission = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        pick = (ImageView)findViewById(R.id.pickImage);
+        pick.setOnClickListener(new View.OnClickListener(){
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v){
+                int picd = 0;
+                if (picd == 0){
+                    if(!checkCameraPermission()){
+                        requestCameraPermission();
+                    }
+                    else {
+                        pickFromGallery();
+                    }
+                }
+                else if (picd == 1){
+                    if (!checkStoragePermission()){
+                        requestStoragePermission();
+                    }
+                    else{
+                        pickFromGallery();
+                    }
+                }
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestStoragePermission() {
+        requestPermissions(cameraPermission,STORAGE_REQUEST);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result= ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void pickFromGallery() {
+        CropImage.activity().start(this);
+    }
+
+    private boolean checkCameraPermission() {
+        boolean result= ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestCameraPermission() {
+        requestPermissions(cameraPermission,CAMERA_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode==RESULT_OK){
+                Uri resultUri=result.getUri();
+                Picasso.with(this).load(resultUri).into(pick);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case CAMERA_REQUEST: {
+                if (grantResults.length > 0) {
+                    boolean camera_accepted = grantResults[0] == (PackageManager.PERMISSION_GRANTED);
+                    boolean storage_accepted = grantResults[1] == (PackageManager.PERMISSION_GRANTED);
+
+                    if (camera_accepted && storage_accepted) {
+                        pickFromGallery();
+                    } else {
+                        Toast.makeText(this, "Please enable camera and storage permissions", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+            case STORAGE_REQUEST:{
+                if (grantResults.length > 0) {
+                    boolean storage_accepted = grantResults[0] == (PackageManager.PERMISSION_GRANTED);
+
+                    if (storage_accepted) {
+                        pickFromGallery();
+                    } else {
+                        Toast.makeText(this, "Please enable storage permissions", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+        }
     }
 }
