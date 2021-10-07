73071c3a921ea2e6b1d149d6e6ee2cd0c04c4f26
Ansel-Hong <anselhonggibby@gmail.com>
2021-10-07 15:59:45 +0800

    GMTGAN-67 Added mock code to send new user profile info

--- BeingSeenApp/app/src/main/java/com/example/myapplication/ProfileInfo.java
+++ BeingSeenApp/app/src/main/java/com/example/myapplication/ProfileInfo.java
@@ -4,6 +4,19 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 
@@ -43,5 +56,96 @@ public class ProfileInfo {
         byte[] pfpbytes = Base64.decode(encodedImage, Base64.DEFAULT);
         Bitmap bmp = BitmapFactory.decodeByteArray(pfpbytes, 0, pfpbytes.length);
         return bmp;
+    }
+
+    public void getInfoFromDb(AppCompatActivity callingActivity){
+
+        RequestQueue queue = Volley.newRequestQueue(callingActivity);
+
+        // a simple API to test if we can connect to backend
+        String url = "http://10.0.2.2:8080/test";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> {
+                        // Format response json data
+//                    this.username = username;
+//                    this.desc = desc;
+//                    this.pfp = pfp;
+                }, error -> this.username = null);
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    public void sendInfoToDb(AppCompatActivity callingActivity){
+        RequestQueue queue = Volley.newRequestQueue(callingActivity);
+
+        // a simple API to test if we can connect to backend
+        String url = "http://10.0.2.2:8080/test";
+
+        // Request a string response from the provided URL.
+//        StringRequest stringRequest = new StringRequest(Request.Method.PUT, url,
+//                response -> {
+//                    // Format response json data
+////                    this.username = username;
+////                    this.desc = desc;
+////                    this.pfp = pfp;
+//                }, error -> this.username = null);
+
+        //create json object
+        final JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("profileName", this.username);
+            jsonObject.put("profileDesc", this.userDescription);
+            jsonObject.put("pfpEncrypted", this.profileImage);
+        } catch (JSONException e) {
+            // handle exception
+        }
+
+        //add json object
+//        JsonObjectRequest putRequest = new JsonObjectRequest(Request.Method.PUT, url, jsonObject,
+//                new Response.Listener<JSONObject>()
+//                {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        // response
+//                        Log.d("Response", response.toString());
+//                    }
+//                },
+//                new Response.ErrorListener()
+//                {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        // error
+//                        Log.d("Error.Response", error.toString());
+//                    }
+//                }
+//        ) {
+//
+//            @Override
+//            public Map<String, String> getHeaders()
+//            {
+//                Map<String, String> headers = new HashMap<String, String>();
+//                headers.put("Content-Type", "application/json");
+//                headers.put("Accept", "application/json");
+//                return headers;
+//            }
+//
+//            @Override
+//            public byte[] getBody() {
+//
+//                try {
+//                    Log.i("json", jsonObject.toString());
+//                    return jsonObject.toString().getBytes("UTF-8");
+//                } catch (UnsupportedEncodingException e) {
+//                    e.printStackTrace();
+//                }
+//                return null;
+//            }
+//        };
+
+        // Add the request to the RequestQueue.
+//        queue.add(putRequest);
     }
 }
