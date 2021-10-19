242f6e17760824a89ca717063fd73bd00a242ead
Suguru Seo <suguru.seo@gmail.com>
2021-10-19 13:28:23 +0900

    GMTGAN-77 Fix Conflict in Volleycallback

--- BeingSeenApp/app/src/main/java/com/example/myapplication/VolleyCallBack.java
+++ BeingSeenApp/app/src/main/java/com/example/myapplication/VolleyCallBack.java
@@ -1,10 +1,7 @@
 package com.example.myapplication;
 
-<<<<<<< HEAD
 // Idea to handle volley response from:   On Oct 12
 // https://stackoverflow.com/questions/49342841/android-wait-for-volley-response-for-continue
-=======
->>>>>>> 48eef3cbca31706260068cb8a879793797057e56
 public interface VolleyCallBack {
     void onSuccess();
 }
