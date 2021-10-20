966f8e1b138a3a92252e576133d54e4f6db5678b
Ansel-Hong <anselhonggibby@gmail.com>
2021-10-21 02:44:31 +0800

    Added frontend ui from old GMTGAN branch

---
+++ BeingSeenApp/app/src/main/java/com/example/myapplication/DonorUiFrontend/DnUserDonationActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.DonorUiFrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class DnUserDonationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dn_user_donation);
+    }
+}
