6f00d58a5406145d8a81ff8595a1c3dba1dbd188
Ansel-Hong <anselhonggibby@gmail.com>
2021-10-21 17:56:06 +0800

    Added error handling for donation page

--- BeingSeenApp/app/src/main/java/com/example/myapplication/DonorUiFrontend/DnUserDonoActivity.java
+++ BeingSeenApp/app/src/main/java/com/example/myapplication/DonorUiFrontend/DnUserDonoActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +25,12 @@ public class DnUserDonoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dn_user_dono);
 
+
+        //For future place code to set to donate to user image and name here
+        // <CODE>
+
+
+
         //donate to someone button
         final Button donateButton = (Button) findViewById(R.id.donateConfirm);
 
@@ -31,50 +38,7 @@ public class DnUserDonoActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-
-                // After clicking save all objects on screen into a object
-                EditText sendingTo = (EditText)findViewById(R.id.donateUsername);
-                EditText donoDesc = (EditText)findViewById(R.id.DnDonoText);
-                EditText amount = (EditText)findViewById(R.id.dnDonoAmt);
-
-                Transaction transactionObj = new Transaction();
-
-                String receiver = sendingTo.getText().toString();
-                String comment = donoDesc.getText().toString();
-                Long amt = Long.parseLong(amount.getText().toString());
-
-                transactionObj.makeDnDonationTransaction(receiver,comment,amt,DnUserDonoActivity.this,
-                        new VolleyCallBack() {
-                            @Override
-                            public void onSuccess() {
-
-                                //make toast after success
-                                Toast toast = Toast.makeText(getApplicationContext(), "Donation successful", Toast.LENGTH_LONG);
-                                toast.show();
-
-                                Intent i = new Intent(getApplicationContext(), DnUserProfileViewBalanceActivity.class);
-                                startActivity(i);
-                            }
-
-                            @Override
-                            public void onFailure() {
-
-                                //make toast after fail
-                                Toast toast = Toast.makeText(getApplicationContext(), "Donation Failed", Toast.LENGTH_LONG);
-                                toast.show();
-
-                                Intent i = new Intent(getApplicationContext(), DnUserProfileViewBalanceActivity.class);
-                                startActivity(i);
-                            }
-                        });
-
-
-//                //make toast after fail
-//                Toast toast = Toast.makeText(getApplicationContext(), "Donation Failed", Toast.LENGTH_LONG);
-//                toast.show();
-//
-//                Intent i = new Intent(getApplicationContext(), DnUserProfileViewBalanceActivity.class);
-//                startActivity(i);
+                sendDono(view);
             }
         });
 
@@ -91,5 +55,71 @@ public class DnUserDonoActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+
+    public void sendDono(View view) {
+        // for future pass in username of receiver (e.g. testuser1)
+        // then set as fixed textbox same retreival but is fixed
+        EditText sendingTo = (EditText)findViewById(R.id.donateUsername);
+
+        // After clicking save all objects on screen into a object
+        EditText donoDesc = (EditText)findViewById(R.id.DnDonoText);
+        EditText amount = (EditText)findViewById(R.id.dnDonoAmt);
+
+        Transaction transactionObj = new Transaction();
+
+
+        String receiver = "";
+        receiver = sendingTo.getText().toString();
+        String comment = "";
+        comment = donoDesc.getText().toString();
+        Long amt = 0L;
+        if(!amount.getText().toString().equals("")) {
+            amt = Long.parseLong(amount.getText().toString());
+        }
+
+        if (receiver.equals("")) {
+            Log.i("TRYNA", "HI");
+            sendingTo.setError("Please enter a username");
+            sendingTo.requestFocus();
+            return;
+        }
+
+        if (amt == null || amt <= 0) {
+            amount.setError("Please enter a nonzero amount to donate");
+            amount.requestFocus();
+            return;
+        }
+
+        if (  !( (amt == null || amt <= 0) || (receiver.isEmpty()) )  ) {
+            transactionObj.makeDnDonationTransaction(receiver, comment, amt, DnUserDonoActivity.this,
+                    new VolleyCallBack() {
+                        @Override
+                        public void onSuccess() {
+
+                            //make toast after success
+                            Toast toast = Toast.makeText(getApplicationContext(), "Donation successful", Toast.LENGTH_LONG);
+                            toast.show();
+
+                            Intent i = new Intent(getApplicationContext(), DnUserProfileViewBalanceActivity.class);
+                            startActivity(i);
+                        }
+
+                        @Override
+                        public void onFailure() {
+
+                            //make toast after fail
+                            Toast toast = Toast.makeText(getApplicationContext(),
+                                    "Donation Failed: Make sure the user has a homeless youth account", Toast.LENGTH_LONG);
+                            toast.show();
+
+                            Intent i = new Intent(getApplicationContext(), DnUserProfileViewBalanceActivity.class);
+                            startActivity(i);
+                        }
+                    });
+        }
+
+        return;
     }
 }
