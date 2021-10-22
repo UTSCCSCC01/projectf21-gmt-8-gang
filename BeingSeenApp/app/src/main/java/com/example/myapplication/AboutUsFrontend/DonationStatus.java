0dfc2ae9d0e7f5c26589f8576f87a82f1bb3fc96
YajuuVito <90756129+YajuuVito@users.noreply.github.com>
2021-10-22 18:23:27 +0800

    GMTGAN-76 Update donation to beingseen(#49)

--- BeingSeenApp/app/src/main/java/com/example/myapplication/AboutUsFrontend/DonationStatus.java
+++ BeingSeenApp/app/src/main/java/com/example/myapplication/AboutUsFrontend/DonationStatus.java
@@ -17,13 +17,8 @@ public class DonationStatus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation_status);
-        String trId=getIntent().getStringExtra("transactionId");
-        TextView transactionId=(TextView) findViewById(R.id.dns_transactionId);
-        transactionId.setText(trId);
         TextView transactionStatus=(TextView) findViewById(R.id.dns_status);
-        transactionStatus.setText("success or fail");
         TextView transactionInfo=(TextView) findViewById(R.id.dns_info);
-        transactionInfo.setText("code return by db: explaination of code");
         Button backHome=(Button) findViewById(R.id.dns_backHome);
         backHome.setOnClickListener(new View.OnClickListener() {
             @Override
