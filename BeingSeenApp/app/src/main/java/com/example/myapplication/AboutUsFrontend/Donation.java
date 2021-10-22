ebc38993406a5ffe09b9dcb317463d2bb9f4fa1a
YajuuVito <vittorio_zhang@foxmail.com>
2021-10-22 15:44:25 +0800

    GMTGAN-76 removing verification before donation
    Because unable to fix verification bugs

--- BeingSeenApp/app/src/main/java/com/example/myapplication/AboutUsFrontend/Donation.java
+++ BeingSeenApp/app/src/main/java/com/example/myapplication/AboutUsFrontend/Donation.java
@@ -28,8 +28,6 @@ public class Donation extends AppCompatActivity {
         TextView receiver=(TextView) findViewById(R.id.dnt_reciever);
         receiver.setText(receiverId);
         TextInputEditText amountField=(TextInputEditText) findViewById(R.id.dnt_amount);
-        TextInputEditText usernameField=(TextInputEditText) findViewById(R.id.dnt_username);
-        TextInputEditText passwordField=(TextInputEditText) findViewById(R.id.dnt_password);
         TextInputEditText commentField=(TextInputEditText) findViewById(R.id.dnt_comment);
         Button confirm=(Button) findViewById(R.id.dnt_confirm);
         confirm.setOnClickListener(new View.OnClickListener() {
@@ -37,8 +35,6 @@ public class Donation extends AppCompatActivity {
             public void onClick(View view) {
                 //read input
                 String amountStirng=amountField.getText().toString();
-                String username = usernameField.getText().toString();
-                String password = passwordField.getText().toString();
                 String comment  = commentField.getText().toString();
                 //check if required input empty
                 if (amountStirng.isEmpty()) {
@@ -52,61 +48,16 @@ public class Donation extends AppCompatActivity {
                     amountField.requestFocus();
                     return;
                 }
-                if (username.isEmpty()) {
-                    usernameField.setError("please enter a username");
-                    usernameField.requestFocus();
-                    return;
-                }
-                if (password.isEmpty()) {
-                    passwordField.setError("please enter a password");
-                    passwordField.requestFocus();
-                    return;
-                }
-                //verify username&password
-                DonorVerify verification=new DonorVerify(Donation.this);
-                verification.logIn(username,password);
-                //case1: not a valid combination
-                if(!verification.isValid()){
-                    new AlertDialog.Builder(Donation.this)
-                            .setTitle("Invalid Username or Password")
-                            .setMessage("Invalid username password pair")
-                            .setPositiveButton("Confirm",null)
-                            .show();
-                }
-                //case2: is a donor, do transaction
-                else if(verification.getRole().equals("ROLE_DONOR")){
-                    //old method using transactionInfo in comment:
-                    //TransactionInfo transaction=new TransactionInfo(username,receiverId,amount,comment);
-                    //transaction.setToken(token);
-                    //transaction.sendInfoToDb(Donation.this, new VolleyCallBack() {
-                        //@Override
-                        //only go to donation status if success
-                        //public void onSuccess() {
-                            //Intent intent=new Intent();
-                           // intent.setClass(Donation.this, DonationStatus.class);
-                           // startActivity(intent);
-                        //}
-                    //});
-                    //new method using Transaction:
-                    Transaction transaction=new Transaction();
-                    transaction.makeDnDonationTransaction(receiverId, comment, amount,
-                            Donation.this, new VolleyCallBack() {
-                        @Override
-                        public void onSuccess() {
-                            Intent intent=new Intent();
-                            intent.setClass(Donation.this, DonationStatus.class);
-                            startActivity(intent);
-                        }
-                    });
-                }
-                //case3: not a donor
-                else{
-                    new AlertDialog.Builder(Donation.this)
-                            .setTitle("Invalid role")
-                            .setMessage("You are not a donor")
-                            .setPositiveButton("Confirm",null)
-                            .show();
-                }
+                Transaction transaction=new Transaction();
+                transaction.makeDnDonationTransaction(receiverId, comment, amount,
+                        Donation.this, new VolleyCallBack() {
+                            @Override
+                            public void onSuccess() {
+                                Intent intent=new Intent();
+                                intent.setClass(Donation.this, DonationStatus.class);
+                                startActivity(intent);
+                            }
+                        });
             }
         });
     }
