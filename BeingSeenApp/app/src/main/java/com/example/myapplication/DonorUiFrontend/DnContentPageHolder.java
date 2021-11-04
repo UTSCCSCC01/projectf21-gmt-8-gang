fd2d6b4949f86a8455e51d72f1535bb47174bd00
Suguru Seo <suguru.seo@gmail.com>
2021-11-04 22:20:03 +0900

    Revert "GMTGAN-91 donation goal page UI (#62)" (#63)
    This reverts commit 272d95f7e31f87b7ca1409c4ab8c166f67ea62da.

--- BeingSeenApp/app/src/main/java/com/example/myapplication/DonorUiFrontend/DnContentPageHolder.java
+++ BeingSeenApp/app/src/main/java/com/example/myapplication/DonorUiFrontend/DnContentPageHolder.java
@@ -2,7 +2,6 @@ package com.example.myapplication.DonorUiFrontend;
 
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,26 +11,20 @@ import com.example.myapplication.R;
 
 public class DnContentPageHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    static ImageView proPic;
-    TextView username, title, description, progress, percentage;
-    ProgressBar progressBar;
+    ImageView proPic;
+    TextView username, title, description;
 
-    ItemClickListener itemClickListener;
 //    private AdapterView.OnItemClickListener listener;
-    //DnContentPageAdapter.ContentPageRecyclerViewClickListener itemClickListener;
+    private DnContentPageAdapter.ContentPageRecyclerViewClickListener listener;
 
-    public DnContentPageHolder(@NonNull View itemView, ItemClickListener listener) {
+    public DnContentPageHolder(@NonNull View itemView, DnContentPageAdapter.ContentPageRecyclerViewClickListener listener) {
         super(itemView);
 
-        //this.proPic = itemView.findViewById(R.id.proPic);
+        this.proPic = itemView.findViewById(R.id.proPic);
         this.username = itemView.findViewById(R.id.homelessUsername);
         this.title = itemView.findViewById(R.id.goalTitle);
         this.description = itemView.findViewById(R.id.gDescription);
-        this.progress = itemView.findViewById(R.id.progress);
-        this.percentage = itemView.findViewById(R.id.percentage);
-        //this.progressBar = itemView.findViewById(R.id.progressBar);
-
-        this.itemClickListener = listener;
+        this.listener = listener;
 
         itemView.setOnClickListener(this);
 
@@ -39,11 +32,6 @@ public class DnContentPageHolder extends RecyclerView.ViewHolder implements View
 
     @Override
     public void onClick(View view) {
-        this.itemClickListener.onItemClickListener(view, getLayoutPosition());
+        listener.onClick(view, getAdapterPosition());
     }
-
-    public void setItemClickListener(ItemClickListener itemClickListener){
-        this.itemClickListener=itemClickListener;
-    }
-
 }
--- BeingSeenApp/app/src/main/java/com/example/myapplication/DonorUiFrontend/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.DonorUiFrontend;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onItemClickListener(View view, int position);
-}
