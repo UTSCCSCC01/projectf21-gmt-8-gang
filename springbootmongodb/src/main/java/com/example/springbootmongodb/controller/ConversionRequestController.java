199aa965342d6bb3cb835a2077df55383624b50a
Hsu Shen (Angus) Lee <62867164+drunkint@users.noreply.github.com>
2021-11-10 11:14:39 +0800

    GMTGAN-183 added get API for allConversionRequests, and changed naming.

--- springbootmongodb/src/main/java/com/example/springbootmongodb/controller/ConversionRequestController.java
+++ springbootmongodb/src/main/java/com/example/springbootmongodb/controller/ConversionRequestController.java
@@ -4,22 +4,28 @@ package com.example.springbootmongodb.controller;
 import com.example.springbootmongodb.model.AppUser;
 import com.example.springbootmongodb.model.ConversionRequest;
 import com.example.springbootmongodb.repository.AccountRepository;
-import com.example.springbootmongodb.repository.ConversionRequstRepository;
+import com.example.springbootmongodb.repository.ConversionRequestRepository;
 import com.example.springbootmongodb.request.ConversionRequestRequest;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 public class ConversionRequestController {
 
     @Autowired
-    private ConversionRequstRepository conversionRequstRepository;
+    private ConversionRequestRepository conversionRequestRepository;
 
     @Autowired
     private AccountRepository accountRepository;
@@ -40,7 +46,7 @@ public class ConversionRequestController {
 
         try {
             ConversionRequest a = new ConversionRequest(username, email, amount, false);
-            conversionRequstRepository.save(a);
+            conversionRequestRepository.save(a);
         } catch (Exception e) {
             System.out.println("error on saving conversion request");
             return new ResponseEntity<>("error on saving conversion request", HttpStatus.INTERNAL_SERVER_ERROR);
@@ -51,4 +57,18 @@ public class ConversionRequestController {
 
     }
 
+    @GetMapping("/allConversionRequests")
+    private ResponseEntity<?> getAllConversionRequests(){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        List<?> conversionRequests = new ArrayList<>();
+        try {
+            conversionRequests = conversionRequestRepository.findConversionRequestByUsername(username);
+            Collections.reverse(conversionRequests);
+        } catch (Exception e) {
+            System.out.println("error on getting list of conversion request");
+            return new ResponseEntity<>("error on getting list of conversion request", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        System.out.println("successfully get conversion requests");
+        return new ResponseEntity<>(conversionRequests, HttpStatus.OK);
+    }
 }
